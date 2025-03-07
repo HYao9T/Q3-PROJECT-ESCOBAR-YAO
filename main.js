fe0c4a667bfcb0462ea52c03e63936fb3de8879a
harhar <yao.hgk@obmontessori.edu.ph>
2025-03-07 21:14:42 +0800

    Update main.js

--- main.js
+++ main.js
@@ -1,16 +1,12 @@
+function discountPrice() {
+    let FeePrice = document.getElementById('price-button').value;
+    let price = 28;
+    let discount = 0;
 
-	function discountPrice() {
-		    let FeePrice = document.getElementById('price-button').value;
-		    let price = 28;
-		    let discount = 0;
+    if (FeePrice === 'discounted_f') {
+        discount = 0.2;
+    } else if (FeePrice === 'regular_f') {
+        discount = 0;
+    }
 
-		    if (seatType === 'discounted_f') {
-		        discount = 0.2;
-
-		        if (seatType === 'regular_f') {
-		        discount = 0;
-		    }
-
-		    let discount_price = price - (price * discount);
-		    window.alert('Your fee is ' + discount_price.toFixed(2));
-		}
+}	
