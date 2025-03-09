434792c946c48f663003b32974327a568d3df246
SEscobar_9T <escobar.sa@obmontessori.edu.ph>
2025-03-10 07:50:05 +0800

    Update main.js

--- main.js
+++ main.js
@@ -1,1 +1,17 @@
 
+// ticket price 
+
+	function checkDiscount() {
+		let leaveType = document.getElementById("leave_from").value;
+		let userType = document.getElementById("options").value;
+		//
+		let originalPrice;
+		if (leaveType === "Roosevelt") {originalPrice = 28;}
+		if (leaveType === "Santolan") {originalPrice = 28;};
+		if (leaveType === "North Avenue") {originalPrice = 28;}
+		//
+		let discountMultiplier = (userType === "student") * 0.2;
+		let discountedPrice = originalPrice - (originalPrice * discountMultiplier);
+
+		window.alert('Your ticket price is ₱' + discountedPrice.toFixed(2) + "." + " Please proceed to the next step."); 
+	}
