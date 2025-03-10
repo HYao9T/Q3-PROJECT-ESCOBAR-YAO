07cb355f23ff3ed51d41a670e46bc65f329ce808
harhar <yao.hgk@obmontessori.edu.ph>
2025-03-10 10:13:19 +0800

    Update main.js

--- main.js
+++ main.js
@@ -1,17 +1,14 @@
+function discountFare() {
+		    let  = document.getElementById('student').value;
+            let  = document.getElementById('senior').value;
+            let price = 100;
+            let discount = 0;
 
-// ticket price 
+            if (seatType === '') {
+                discount = 0.2;
+            }
 
-	function checkDiscount() {
-		let leaveType = document.getElementById("leave_from").value;
-		let userType = document.getElementById("options").value;
-		//
-		let originalPrice;
-		if (leaveType === "Roosevelt") {originalPrice = 28;}
-		if (leaveType === "Santolan") {originalPrice = 28;};
-		if (leaveType === "North Avenue") {originalPrice = 28;}
-		//
-		let discountMultiplier = (userType === "student") * 0.2;
-		let discountedPrice = originalPrice - (originalPrice * discountMultiplier);
 
-		window.alert('Your ticket price is ₱' + discountedPrice.toFixed(2) + "." + " Please proceed to the next step."); 
-	}
+		    let discount_price = price - (price * discount);
+		    window.alert('Your total fare is ' + discount_price.toFixed(2) + '. Please proceed to the next step');
+		}
