c3a073040e5cd47ec2b55d91a4c4b49bbc8b5c9e
harhar <yao.hgk@obmontessori.edu.ph>
2025-03-10 13:41:04 +0800

    Update main.js

--- main.js
+++ main.js
@@ -1,15 +1,15 @@
 function discountFare() {
+    let seatType = document.getElementById("discountID").value;
+    let price = 28;
+    let discount = 0;
 
-    let seatType = document.getElementById('discountID').value;
-    let price = 0;
-    if (seatType === 'regular') {price = 28;}
-    if (seatType === 'student') {price = 28;}
-    if (seatType === 'senior') {price = 28;}
+    if (seatType === "student" || seatType === "senior") {
+        discount = 0.2;
+    }
 
-    let discountMultiplier = (seatType === "student") * 0.2;
-    let discountPrice = price - (price * discountMultiplier);
-    let discountPrice_round = discountprice.toFixed(2)
+    let discountPrice = price - (price * discount);
+    
+    document.getElementById("discountFare").innerHTML = "Your discounted fare is: ₱" + discountPrice.toFixed(2);
 
-    alert('Your total price is ₱' + discountPrice_round + '. Please proceed to the next section.');
-
+    window.alert("Your total price is ₱" + discountPrice.toFixed(2) + ". Please proceed to the next section.");
 }
